c80fe40b4afbe33bc9475763ead92a1cd5f46f35
Smirow <magued1509@gmail.com>
2022-10-30 13:03:59 +0100

    test

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -1,3 +1,14 @@
-export default function TestPage() {
-  return <div>TEST PAGE ID</div>;
+import Link from "next/link";
+
+export default function TestPage({
+  params,
+}: {
+  params: { [n: string]: string };
+}) {
+  return (
+    <div>
+      TEST PAGE {params.id}
+      <Link href={`/test/${params.id}`}></Link>
+    </div>
+  );
 }
