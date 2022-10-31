1fd0eb865bdbf5a2b674dc1d35bf0f4db904465c
Smirow <magued1509@gmail.com>
2022-10-31 11:22:12 +0100

    test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,6 +4,7 @@ export default function TestPage() {
   return (
     <div>
       <Link href={`test/${1}`}>Main</Link>
+      <br></br>
       <Link href={`test/${1}/second`}>Second</Link>
     </div>
   );
